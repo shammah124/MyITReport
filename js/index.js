4babc4c3cc092898cc4e1a8a596f0e99268aa63b
shammah124 <99246318+shammah124@users.noreply.github.com>
2022-02-13 23:55:36 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function myReport() {
+    var x = document.getElementById('myTable')
+    if (x.style.display === "block") {
+        x.style.display = "none"
+    } else {
+        x.style.display = "block"
+    }
+}
+function showAlert() {
+    alert("To view my Week 5 report, click on Week 5"); 
+}
